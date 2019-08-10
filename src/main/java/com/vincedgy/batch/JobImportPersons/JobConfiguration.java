7710f63c1584c53489ffc429913415d2681a856a
Vincent Dagoury ☁ <vincent.dagoury@gmail.com>
2019-08-10 22:58:38 +0200

    Refactoring again

--- src/main/java/com/vincedgy/batch/JobImportPersons/JobConfiguration.java
+++ src/main/java/com/vincedgy/batch/JobImportPersons/JobConfiguration.java
@@ -5,31 +5,18 @@ import com.vincedgy.batch.entity.PersonRedis;
 import com.vincedgy.batch.entity.PersonRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.batch.core.*;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.JobScope;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.*;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.listener.JobExecutionListenerSupport;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.FlatFileParseException;
-import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.mapping.FieldSetMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.item.file.transform.FieldSet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Component;
-
-import java.util.concurrent.atomic.AtomicReference;
+import org.springframework.core.io.Resource;
 
 @Configuration
 @EnableBatchProcessing
@@ -38,89 +25,45 @@ public class JobConfiguration {
 
     final private JobBuilderFactory jbf;
     final private StepBuilderFactory sbf;
-    final private String filename;
     final private int chunk;
+    final private String fileName;
 
     @Autowired
     PersonRepository personRepository;
 
     JobConfiguration(final JobBuilderFactory jbf,
                      final StepBuilderFactory sbf,
-                     @Value("${application.jobImportPersons.filename:MOCK_DATA_TEST.csv}") final String filename,
+                     @Value("${application.jobImportPersons.filename:MOCK_DATA.csv}") final String fileName,
                      @Value("${application.jobImportPersons.chunk:1000}") final int chunk) {
         this.jbf = jbf;
         this.sbf = sbf;
-        this.filename = filename;
         this.chunk = chunk;
+        this.fileName = fileName;
     }
 
     //--------------------------------------------------
     // READER
     //--------------------------------------------------
 
-    private FlatFileItemReader<Person> reader(String s) {
+    @Bean
+    public FlatFileItemReader<Person> filePersonReader() {
         return new FlatFileItemReaderBuilder<Person>()
                 .name("dataItemReader")
-                .resource(new ClassPathResource(filename))
+                .resource(new ClassPathResource(fileName))
                 .delimited()
                 .names(new String[]{"id", "firstName", "lastName", "email", "gender", "ipAddress"})
-                .lineMapper(lineMapper())
-                .fieldSetMapper(new BeanWrapperFieldSetMapper<>() {{
-                    setTargetType(Person.class);
-                }})
+                .fieldSetMapper( fieldSet -> {
+                    Person p = new Person();
+                    p.setId(fieldSet.readLong("id"));
+                    p.setGender(fieldSet.readString("gender"));
+                    p.setEmail(fieldSet.readString("email"));
+                    p.setFirstName(fieldSet.readString("firstName"));
+                    p.setLastName(fieldSet.readString("lastName"));
+                    p.setIpAddress(fieldSet.readString("ipAddress"));
+                    return p;
+                })
                 .build();
     }
-
-    private LineMapper<Person> lineMapper() {
-        final DefaultLineMapper<Person> lineMapper = new DefaultLineMapper<>();
-        final DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
-        final PersonFieldSetMapper fieldSetMapper = new PersonFieldSetMapper();
-
-        lineTokenizer.setDelimiter(",");
-        lineTokenizer.setStrict(false);
-
-        lineTokenizer.setNames("id", "firstName", "lastName", "email", "gender", "ipAddress");
-
-        lineMapper.setLineTokenizer(lineTokenizer);
-        lineMapper.setFieldSetMapper(fieldSetMapper);
-
-        return lineMapper;
-    }
-
-    @Component
-    static class PersonFieldSetMapper implements FieldSetMapper<Person> {
-        @Override
-        public Person mapFieldSet(FieldSet fieldSet) {
-            final Person p = new Person();
-            p.setId(fieldSet.readLong("id"));
-            p.setGender(fieldSet.readString("gender"));
-            p.setEmail(fieldSet.readString("email"));
-            p.setFirstName(fieldSet.readString("firstName"));
-            p.setLastName(fieldSet.readString("lastName"));
-            p.setIpAddress(fieldSet.readString("ipAddress"));
-            return p;
-        }
-    }
-
-    @Component
-    static class JobListenerImportPersons extends JobExecutionListenerSupport {
-        final private PersonRepository repository;
-
-        JobListenerImportPersons(final PersonRepository repository) {
-            this.repository = repository;
-        }
-
-        @Override
-        public void afterJob(final JobExecution jobExecution) {
-            if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
-                log.info("!!! JOB FINISHED! Time to verify the results");
-                AtomicReference<Long> count = new AtomicReference<>(0L);
-                repository.findAll().forEach(personRedis -> count.getAndSet(count.get() + 1));
-                log.info("Found " + count.toString() + " person(s) in Redis database");
-            }
-        }
-    }
-
 
     //--------------------------------------------------
     // STEPs
@@ -130,7 +73,7 @@ public class JobConfiguration {
 
         return sbf.get("1-InsertFromCSVFileToRedisDb")
                 .<Person, PersonRedis>chunk(chunk)
-                .reader(reader(jobParameters.getString("fileName")))
+                .reader(filePersonReader())
                 .processor(new PersonProcessor())
                 .writer(writer)
                 .faultTolerant()
@@ -167,9 +110,9 @@ public class JobConfiguration {
     }
 
     @Bean
-    JobParameters jobParameters() {
+    JobParameters jobParameters(@Value("${application.jobImportPersons.filename}") final String fileName) {
         return new JobParametersBuilder()
-                .addString("fileName", filename, true)
+                .addParameter("fileName", new JobParameter(fileName))
                 .toJobParameters();
     }
 
