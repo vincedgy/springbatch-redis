63950e717ed652c1e5f1cea55e7955402431293a
Vincent <vincent.dagoury@gmail.com>
2019-08-15 15:49:14 +0200

    Refactoring with a proper Job Configuration class and a working application.properties

--- src/main/java/com/vincedgy/batch/BatchApplication.java
+++ src/main/java/com/vincedgy/batch/BatchApplication.java
@@ -23,9 +23,7 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicReference;
 
-@EnableBatchProcessing
 @SpringBootApplication
-@Log4j2
 public class BatchApplication {
     @Bean
     LettuceConnectionFactory lettuceConnectionFactory() {
@@ -35,85 +33,6 @@ public class BatchApplication {
     public static void main(String[] args) {
         SpringApplication.run(BatchApplication.class, args);
     }
-
-    @Bean
-    public ItemReader<Person> filePersonReader() {
-        return new FlatFileItemReaderBuilder<Person>()
-                .name("dataItemReader")
-                .resource(new ClassPathResource("MOCK_DATA.csv"))
-                .delimited().delimiter(",").names(new String[]{"id", "firstName", "lastName", "email", "gender", "ipAddress"})
-                .fieldSetMapper(fieldSet -> {
-                    Person p = new Person();
-                    p.setId(fieldSet.readLong("id"));
-                    p.setGender(fieldSet.readString("gender"));
-                    p.setEmail(fieldSet.readString("email"));
-                    p.setFirstName(fieldSet.readString("firstName"));
-                    p.setLastName(fieldSet.readString("lastName"));
-                    p.setIpAddress(fieldSet.readString("ipAddress"));
-                    return p;
-                })
-                .build();
-    }
-
-    //--------------------------------------------------
-    // Job Definition
-    //--------------------------------------------------
-
-    @Bean
-    public Job importPersonJob(JobBuilderFactory jbf,
-                               StepBuilderFactory sbf,
-                               PersonRepository personRepository
-    ) {
-
-        Step step0 = sbf.get("0-RemoveAllData")
-                .tasklet((stepContribution, chunkContext) -> {
-                    personRepository.deleteAll();
-                    return RepeatStatus.FINISHED;
-                })
-                .startLimit(1)
-                .allowStartIfComplete(true)
-                .build();
-
-        Step step1 = sbf.get("1-InsertFromCSVFileToRedisDb")
-                .<Person, PersonRedis>chunk(100)
-                .reader(filePersonReader())
-                .processor((ItemProcessor<Person, PersonRedis>) person -> {
-                    final PersonRedis returnedPerson = new PersonRedis();
-                    returnedPerson.setId(UUID.randomUUID().toString());
-                    returnedPerson.setExtId(person.getId().toString());
-                    returnedPerson.setName(person.getLastName().toUpperCase() + " " + person.getFirstName());
-                    returnedPerson.setGender(person.getGender().equals("Male") ? PersonRedis.Gender.MALE : PersonRedis.Gender.FEMALE);
-                    return returnedPerson;
-                })
-                .writer(list -> list.forEach(personRepository::save))
-                .faultTolerant()
-                .skipLimit(10)
-                .skip(FlatFileParseException.class)
-                .startLimit(10)
-                .build();
-
-        return jbf.get("importPersons")
-                .incrementer(new RunIdIncrementer())
-                .listener(new JobExecutionListener() {
-                    @Override
-                    public void beforeJob(final JobExecution jobExecution) {
-                    }
-
-                    @Override
-                    public void afterJob(final JobExecution jobExecution) {
-                        if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
-                            log.info("!!! JOB FINISHED! Time to verify the results");
-                            AtomicReference<Long> count = new AtomicReference<>(0L);
-                            personRepository.findAll().forEach(personRedis -> count.getAndSet(count.get() + 1));
-                            log.info("Found " + count.toString() + " person(s) in Redis database");
-                        }
-                    }
-                })
-                .start(step0)
-                .next(step1)
-                .build();
-    }
-
 }
 
 
